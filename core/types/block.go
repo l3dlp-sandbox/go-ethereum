0bafb29490751ad807ca1433962d5aef5ae27986
cui <cuiweixie@gmail.com>
2026-04-07 22:04:07 +0800

    core/types: add accessList to WithSeal and WithBody (#34651)
    Co-authored-by: Felix Lange <fjl@twurst.com>

--- core/types/block.go
+++ core/types/block.go
@@ -505,6 +505,7 @@ func (b *Block) WithSeal(header *Header) *Block {
 		transactions: b.transactions,
 		uncles:       b.uncles,
 		withdrawals:  b.withdrawals,
+		accessList:   b.accessList,
 	}
 }
 
@@ -516,6 +517,7 @@ func (b *Block) WithBody(body Body) *Block {
 		transactions: slices.Clone(body.Transactions),
 		uncles:       make([]*Header, len(body.Uncles)),
 		withdrawals:  slices.Clone(body.Withdrawals),
+		accessList:   b.accessList,
 	}
 	for i := range body.Uncles {
 		block.uncles[i] = CopyHeader(body.Uncles[i])
